78433cb89929f67966ea648973935218ab35122c
ImmanuelTambunan <immannueltambunan@gmail.com>
2026-05-09 17:25:38 +0700

    updatte

---
+++ src/academic/model/MataKuliah.java
@@ -0,0 +1,36 @@
+package academic.model;
+
+/**
+ * MataKuliah - Merepresentasikan data mata kuliah.
+ * Menyimpan kode MK, nama, dan jumlah SKS.
+ */
+public class MataKuliah {
+
+    private String kode;
+    private String nama;
+    private int sks;
+
+    public MataKuliah() {}
+
+    public MataKuliah(String kode, String nama, int sks) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+    }
+
+    // --- Getter & Setter ---
+
+    public String getKode() { return kode; }
+    public void setKode(String kode) { this.kode = kode; }
+
+    public String getNama() { return nama; }
+    public void setNama(String nama) { this.nama = nama; }
+
+    public int getSks() { return sks; }
+    public void setSks(int sks) { this.sks = sks; }
+
+    @Override
+    public String toString() {
+        return kode + " | " + nama + " | " + sks + " SKS";
+    }
+}
